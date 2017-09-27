2acf2cafc16e0896bf2bc585f4a92302b8f342cc
Ben Blackman <bblkmn5@gmail.com>
2017-09-27 02:42:32 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -13,9 +13,12 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 
 def speak_to_grandma(phrase)
-  if phrase == phrase.upcase
-  puts "I LOVE YOU TOO PUMPKIN!"
+  if phrase != phrase.upcase()
+    return "HUH?! SPEAK UP, SONNY!"
+elsif phrase == phrase.upcase()
+    return "I LOVE YOU TOO PUMPKIN!"
 else
-  puts "HUH?! SPEAK UP, SONNY!"
+    return "NO, NOT SINCE 1938!"
   end
+end
 end
